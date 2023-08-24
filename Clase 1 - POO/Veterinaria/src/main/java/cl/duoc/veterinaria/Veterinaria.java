e4cdd674dfc6082cf74f6ee8d305af3c457b1179
Sebas Contreras <swf.sebas@gmail.com>
2023-08-23 21:30:32 -0400

    Added example on main

--- Clase 1 - POO/Veterinaria/src/main/java/cl/duoc/veterinaria/Veterinaria.java
+++ Clase 1 - POO/Veterinaria/src/main/java/cl/duoc/veterinaria/Veterinaria.java
@@ -1,8 +1,18 @@
 package cl.duoc.veterinaria;
 
+import cl.duoc.veterinaria.models.Mascota;
+
 public class Veterinaria {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        // Creamos un objeto Mascota
+        Mascota obj = new Mascota();
+        
+        obj.setNroChip(1000);
+        
+        System.out.print("El número de chip de la mascota es: ");
+        System.out.println(obj.getNroChip());
     }
 }
